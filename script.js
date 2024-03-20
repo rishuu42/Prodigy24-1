8fc8339b23bbf3a0e2437c593c7d99039af9b1b0
Rishitt Gupta <114519456+rishuu42@users.noreply.github.com>
2024-03-20 09:28:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var countDownDate = new Date("April 2, 2024 15:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navbarLinks = document.querySelector('.navbar-links');
+
+  menuToggle.addEventListener('click', function () {
+      navbarLinks.classList.toggle('active');
+  });
+});
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+    x.style.transition = "1s ease-in-out";
+  }
+}
