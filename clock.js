c952587ecd7e8164d669c909f389d6fab51d557d
Rishitt Gupta <114519456+rishuu42@users.noreply.github.com>
2024-03-20 09:35:45 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -14,7 +14,7 @@ window.addEventListener('load', () => {
     let totalSeconds;
     
     function init() {
-        totalSeconds = Math.floor((new Date('04/03/2024') - new Date()) / 1000); 
+        totalSeconds = Math.floor((new Date('08/03/2024') - new Date()) / 1000); 
         setTimeLeft();
         let interval = setInterval(() => {
             if (totalSeconds < 0) {
